29ea2e53f9cf5d6fc2ff9273cb487f4ee680585e
khchoi0301 <khchoi0301@gmail.com>
2019-12-24 00:39:44 +0900

    map setting 추가

--- App.tsx
+++ App.tsx
@@ -6,15 +6,25 @@ import MapView from 'react-native-maps';
 // import { MapView } from 'expo'
 
 export default function App() {
+  const defaultCenter = { lat: 37.39, long: 126.999 }
   return (
     <View style={styles.container}>
       <SearchFilter style={styles.searchFilter} />
       {/* <WebViewExample style={styles.webview} /> */}
 
-      <MapView style={{
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
-      }}
+      <MapView
+        style={{
+          width: Dimensions.get('window').width,
+          height: Dimensions.get('window').height,
+        }}
+        // defaultZoom={8}
+        // defaultCenter={{ lat: defaultCenter.lat, lng: defaultCenter.long }}
+        initialRegion={{
+          latitude: defaultCenter.lat,
+          longitude: defaultCenter.long,
+          latitudeDelta: 0.0122,
+          longitudeDelta: 0.0121,
+        }}
       />
     </View>
   );
